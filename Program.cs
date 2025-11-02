c0f2f206c74bfdb3f56b00e1eea203a4a0a60dfa
black <black@example.com>
2025-11-02 20:01:49 +0500

    Greet Black

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace HelloApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("hello everyone");
+            GreetWhite();
+            GreetBlack();
         }
 
         static void GreetWhite()
